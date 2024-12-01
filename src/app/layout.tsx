d3eb72ec0c03fbaae26148ae6aacaeb09fd22d42
Devraj Deshmukh <devrajdeshmukh112@gmail.com>
2024-12-02 01:48:18 +0530

    ui update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Inter } from "@next/font/google";
